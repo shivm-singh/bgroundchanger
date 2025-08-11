b9a4d1b7850a536b14deebf2fa570ba8f6888d02
firstDemoRepo <180681461+abdu1lah26@users.noreply.github.com>
2025-08-11 22:09:19 +0530

    Background Changer Project

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react"
+
+function App() {
+  const [color, setColor] = useState("olive")
+
+  return (
+    <div className="w-full h-screen duration-200" style={{backgroundColor: color}}>
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        {/* learning: why use this way of onClick{} */}
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg  px-3 py-2 rounded-3xl bg-black">
+          <button onClick= {() => setColor("red")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor: "red"}}>Red</button>
+          <button onClick= {() => setColor("green")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor: "green"}}>Green</button>
+          <button onClick= {() => setColor("blue")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor: "blue"}}>Blue</button>
+          <button onClick= {() => setColor("orange")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor: "orange"}}>Orange</button>
+          <button onClick= {() => setColor("brown")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor: "brown"}}>Brown</button>
+          <button onClick= {() => setColor("violet")} className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor: "violet"}}>Violet</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
